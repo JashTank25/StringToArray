5cc9c14e61f179f3349a66e0f32ed33cf72b104d
JashTank25 <jashtank25@gmail.com>
2023-03-17 21:07:37 +0530

    String_to_StringArray

---
+++ StringToArray.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class StringToArray
+{
+	public static void main(String[] args) 
+	{
+		String a="My name is jash";
+		String[] b=a.split(" ");
+		System.out.println(Arrays.toString(b));
+	}
+}
